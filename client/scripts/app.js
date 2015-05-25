af89b9f7bb82c7a193a73f855ec974656f849988
spacemixup <xchrislee@gmail.com>
2015-05-25 15:19:10 -0700

    Should be able to clear message from the DOM / chatroom behaviour

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,40 @@
 // YOUR CODE HERE:
+var app = {};
+var url = 'https://api.parse.com/1/classes/chatterbox';
 
+app.init = function() {
+};
+
+
+app.fetch = function (){
+  var request = $.ajax({
+      type: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('chatterbox: you suck, but you were able to feth - like a dog.');
+      },
+      error: function (data) {
+        console.error('chatterbox: You failed - fetch is not a thing');
+      }
+  });
+};
+
+app.send = function(message) {
+  var request = $.ajax({
+    url: url,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('chatterbox: you suck, but message was sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: You failed - at life');
+    }
+
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').html('');
+};
