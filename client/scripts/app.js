ae9b39210f07246bcd56b849301d8091351e1549
spacemixup <xchrislee@gmail.com>
2015-05-25 20:13:16 -0700

    Basic functionality working.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,75 +1,92 @@
 // YOUR CODE HERE:
-var app = {};
-var url = 'https://api.parse.com/1/classes/chatterbox';
+$(document).ready(function (){
+  var app = {};
+  var url = 'https://api.parse.com/1/classes/chatterbox';
 
-app.init = function() {
-  // debugger;
-  $('.username').on('click', function (){
-    app.addFriend();
+  app.init = function() {
 
-  })
-  $("#send").on('submit', function (){
-    app.handleSubmit();
-  })
-};
+    var messages = app.fetch();
+
+    $('.username').on('click', function (){
+      app.addFriend();
+
+    })
+    $(".submit").on('submit', function (e){
+      e.preventDefault();
+
+      var submittedMessage = $('#message').val();
+      console.log(submittedMessage);
+      app.handleSubmit();
+
+    });
+
+  };
 
 
-app.fetch = function (){
-  var request = $.ajax({
-      type: 'GET',
+  app.fetch = function (){
+    $.ajax({
+        type: 'GET',
+        url: url,
+        contentType: 'application/json',
+        success: function(data) {
+          console.log(data.results);
+          _.each(data.results,function(message){app.addMessage(message)});
+          console.log('chatterbox: you suck, but you were able to fetch - like a dog.');
+        },
+        error: function (data) {
+          console.error('chatterbox: You failed - fetch is not a thing');
+        }
+    });
+  };
+
+  app.send = function(message) {
+    // debugger;
+    console.log(message);
+    var request = $.ajax({
+      url: url,
+      type: 'POST',
+      data: JSON.stringify({
+        text: message,
+        username: window.location.href.split("username=")[1],
+        roomname: 'Tamara rhymes with Orange'
+      }),
       contentType: 'application/json',
       success: function(data) {
-        console.log('chatterbox: you suck, but you were able to feth - like a dog.');
+        console.log('chatterbox: you suck, but message was sent');
+        console.log(data)
       },
       error: function (data) {
-        console.error('chatterbox: You failed - fetch is not a thing');
+        console.error('chatterbox: You failed - at life');
       }
-  });
-};
 
-app.send = function(message) {
-  var request = $.ajax({
-    url: url,
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function(data) {
-      console.log('chatterbox: you suck, but message was sent');
-    },
-    error: function (data) {
-      console.error('chatterbox: You failed - at life');
-    }
+    });
+  };
 
-  });
-};
+  app.clearMessages = function() {
+    $('#chats').html('');
+  };
 
-app.clearMessages = function() {
-  $('#chats').html('');
-};
+  app.addMessage = function(message) {
 
-app.addMessage = function(message) {
+    var $username = '<div class = "username msg-part"> '+ _.escape(message.username) +  '</div>'
+    var $text = '<div class = "messagetext msg-part"> '+ _.escape(message.text) + '</div>'
+    var $roomname = '<div class = "roomname msg-part"> '+ _.escape(message.roomname) + '</div>'
+    var $message = '<div class = "messagebody">'+ $username + ': ' + $text + ' -- from ' + $roomname + '</div>'
+    $('#chats').append($('<div>' + $message + '</div>'));
 
-  var $username = '<div class = "username"> '+ message.username +  '</div>'
-  var $text = '<div class = "messagetext"> '+ message.text + '</div>'
-  var $roomname = '<div class = "roomname"> '+ message.roomname + '</div>'
-  var $message = '<div class = "messagebody">'+ $username + $text + $roomname + '</div>'
-  $('#chats').append($('<div>' + $message + '</div>'));
+  };
 
-};
+  app.addRoom = function(roomName) {
+    $('#roomSelect').append($('<div>' + roomName + '</div>'));
+  };
 
-app.addRoom = function(roomName) {
-  $('#roomSelect').append($('<div>' + roomName + '</div>'));
-};
+  app.addFriend = function(){
+  };
 
-app.addFriend = function(){
-};
+  app.handleSubmit = function (){
+    var message = $('#message').val();
+    app.send(message);
+  };
 
-app.handleSubmit = function (){
-  // var val = $("input[type=submit][clicked=true]").val()
-  var message = $('#message').val();
-  debugger;
-  app.send(message);
-
-
-};
-
+  app.init();
+});
